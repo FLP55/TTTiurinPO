498b55e3838dddba3803348345284107a357979a
p.tiurin <bloodpoker1@gmail.com>
2023-07-27 13:32:57 +0300

    delete prints

--- test_framework/steps_api.py
+++ test_framework/steps_api.py
@@ -16,7 +16,6 @@ class ApiSteps:
         self.request.params['api_key'] = api_key
         self.request.params['earth_date'] = earth_date
         response = self.request.get_data_from_request()
-        print(response)
         CommonChecker.check_status_code_403(response, assertion_message="Статус код 200")
 
     def check_status_code_200(self, sol, camera, page, api_key, earth_date) -> Any:
@@ -26,5 +25,4 @@ class ApiSteps:
         self.request.params['api_key'] = api_key
         self.request.params['earth_date'] = earth_date
         response = self.request.get_data_from_request()
-        print(response.json())
         CommonChecker.check_status_code_200(response, assertion_message="Статус код 403")
